b544c8e4d5ee2470e71415580c133542ef4937e4
Leonardo Santis <leo.psicokinesis@gmail.com>
2014-08-21 23:06:44 -0400

    nave, fondo y balas

--- Assets/Background2Script.cs
+++ Assets/Background2Script.cs
@@ -3,13 +3,35 @@ using System.Collections;
 
 public class Background2Script : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	//the point the background goes back to start
+	private float reset = -5.1f;
 	
-	}
+	//the point where the background goes after a reset
+	private float startPoint = 5f;
+	private float startPointx;
+	private float startPointz;
+	//the speed of movement
+	public float speed;
 	
-	// Update is called once per frame
+	public GameObject otherBackground;
+	
+	
 	void Update () {
-	
+
+		
+		//if the background is at reset point, move it to startPoint
+		if(transform.position.y <= reset) {
+			transform.position = (new Vector3(startPointx, startPoint, startPointz));
+		}
+		
+		//EnemySpawner.speedPlus is a static float
+		
+		//Move the backgound up
+		transform.position -= new Vector3(0,speed,0);
+	}
+
+	void Start(){
+		startPointx = transform.position.x;
+		startPointz = transform.position.z;
 	}
 }
---
+++ Assets/BulletScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3 (0, speed, 0);
+		float hbound = Camera.current.orthographicSize * Camera.current.aspect;
+		float vbound = Camera.current.orthographicSize * Camera.current.aspect;
+
+		if(transform.position.y <= -(vbound + 2)){
+			Destroy (gameObject,0.0f);
+		}
+		if(transform.position.y >= (vbound + 2)){
+			Destroy (gameObject,0.0f);
+		}
+	}
+}
